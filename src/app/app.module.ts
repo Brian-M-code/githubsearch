cc939e21c210add2b195ca39597d1a95c5988682
Brian-M-Code <mutuma.brian@yahoo.com>
2019-12-06 14:49:49 +0300

    declared service properties, of type and getProfile function in the profile service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileService} from './profile.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -8,13 +9,14 @@ import { ProfileComponent } from './profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid= '';
+  private clientsecret= '';
+
+
+  constructor(private http: Http) { 
+    console.log("Service is now ready");
+    this.username = 'Brian-M-Code'
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username+ "?client_id" + this.clientid + "&client_secret=" +this.clientsecret)
+    .map(res => res.json());
+  }
 }
